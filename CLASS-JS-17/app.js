0fe58318f332a7754263364eb5f13aa71d2f4088
faiz <razafaiz815@gmail.com>
2021-03-12 18:15:37 +0500

    App initialized and Navbar added

---
+++ CLASS-JS-17/app.js
@@ -0,0 +1,11 @@
+function makeInvisible(){
+    document.getElementById("image").className += " hidden";
+}
+
+function swapPic(id, source){
+    document.getElementById(id).src = source;
+}
+
+function swapPic2(id, source){
+    document.getElementById(id).src = source;
+}
