fd1c9c2556d853555117f14764693b1a45227e7d
faiz <razafaiz815@gmail.com>
2021-03-25 22:13:30 +0500

    Class 18 added

---
+++ CLASS-18/app.js
@@ -0,0 +1,8 @@
+// var e = document.getElementById("rules");
+// var paragraphs = e.getElementsByTagName("p");
+
+// for (var i = 0; i < 2; i++) {
+//   console.log(paragraphs[i].innerText);
+// }
+
+console.log(document.childNodes[1].childNodes[2].childNodes)
